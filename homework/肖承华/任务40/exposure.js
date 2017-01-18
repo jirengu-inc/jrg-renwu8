e3a6a52a925c52efa4e34895ea31efd0f07f034f
batman-1 <646563557@qq.com>
2017-01-18 23:16:42 +0800

    Update exposure.js

--- homework/肖承华/任务40/exposure.js
+++ homework/肖承华/任务40/exposure.js
@@ -1,31 +1,31 @@
-	Exposure = (function(){
-			var flag;
-			var exposure = {
-				bind:function(target,callback){
-					if (exposure.isShow(target)) {
-						typeof callback === 'function'? callback.call(target):console.log('请传入函数')
-					}
-				},
-
-				one:function(target,callback){
-					if (!flag) {
-						if(exposure.isShow(target)){
-							typeof callback === 'function'? callback.call(target):console.log('请传入函数')
-							flag = true;
-						}
-					}		
-				},
-
-				isShow:function(node){
-					var scrollTop = $(window).scrollTop(),
-						nodeH = node.offset().top,
-						windowH = $(window).height();
-					if (scrollTop + windowH > nodeH ) {
-						return true;
-					}else{
-						return false;
-					}
-				}
+Exposure = (function(){
+	var flag;
+	var exposure = {
+		bind:function(target,callback){
+			if (exposure.isShow(target)) {
+				typeof callback === 'function'? callback.call(target):console.log('请传入函数')
 			}
-			return exposure;
-		})();
+		},
+
+		one:function(target,callback){
+			if (!flag) {
+				if(exposure.isShow(target)){
+					typeof callback === 'function'? callback.call(target):console.log('请传入函数')
+					flag = true;
+				}
+			}		
+		},
+
+		isShow:function(node){
+			var scrollTop = $(window).scrollTop(),
+				nodeH = node.offset().top,
+				windowH = $(window).height();
+			if (scrollTop + windowH > nodeH ) {
+				return true;
+			}else{
+				return false;
+			}
+		}
+	}
+	return exposure;
+})();
