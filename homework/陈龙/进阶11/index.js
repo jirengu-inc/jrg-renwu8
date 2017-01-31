593d3fcf693cfa109dca989aa95801e00ef12498
chenlong <751934754@qq.com>
2017-01-31 08:52:44 +0800

    进阶12

---
+++ homework/陈龙/进阶11/index.js
@@ -0,0 +1,8 @@
+const isAndroid=()=>/android/i.test(navigator.userAgent);
+const isIphone=()=>/iphone/i.test(navigator.userAgent);
+const isIpad=()=>/ipad/i.test(navigator.userAgent);
+const isIOS=()=>/iphone|ipad/i.test(navigator.userAgent);
+
+
+
+
