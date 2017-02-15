f1110731f0da448951b3dc1e18a46d5519d9bb85
fm20115412 <641290900@qq.com>
2017-02-15 22:04:18 +0800

    add files

---
+++ homework/冯敏/高级任务4/a.js
@@ -0,0 +1,10 @@
+/**
+ * Created by fm on 2017/2/12.
+ */
+var people={
+    name:"ruoyu",
+    sayName:function () {
+        console.log(this.name);
+    }
+}
+module.exports=people;
---
+++ homework/冯敏/高级任务4/b.js
@@ -0,0 +1,6 @@
+/**
+ * Created by fm on 2017/2/12.
+ */
+var p=require("./a");
+p.sayName();
+console.log(p.name);
